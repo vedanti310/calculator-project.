66e0608b54497a8b82e173a1a9c23d8a6d2dc267
vedanti310 <vedantimahadik2004@gmail.com>
2024-12-17 14:24:53 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x+y
+
+def substract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def max(x,y,z):
+ m = x
+ if(y>m):
+  m=y
+ if(z>m):
+  m=z
+ return m 
